5e7a9f91b602b35b46680b3c03faf671ab0ed1b3
Daniel Nell <daniel@sapphiregemtech.com>
2021-03-17 10:51:08 +0200

    fixed Migration file

--- db/migrate/20210315070038_add_post_status_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddPostStatusToBlogs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :blogs, :stauts, :integer, default: 0
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_070038) do
+ActiveRecord::Schema.define(version: 2021_03_10_131921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 2021_03_15_070038) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
-    t.integer "stauts", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
